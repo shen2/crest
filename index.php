e1f7b5e93609a349a2133472cb67abf02ece8bdc
shen2 <zhenyupku@gmail.com>
2015-03-11 18:23:25 +0800

    implement POST PATCH DELETE methods

--- index.php
+++ index.php
@@ -2,45 +2,153 @@
 require 'vendor/autoload.php';
 
 $app = new \Slim\Slim(array(
-    'debug'=> true,
-    'mode' => 'development',
-    'http.version' => '1.1'
+	'debug'=> false,
+	'mode' => 'development',
+	'http.version' => '1.1'
 ));
 
 $db = new Cassandra\Connection(['127.0.0.1']);
 
-$app->get('/:keyspace/query', function ($keyspace) use ($app, $db){
-    $db->setKeyspace($keyspace);
-    $cql = $app->request->get('cql');
-    $result = $db->querySync($cql);
-    $app->response->write(json_encode($result->fetchAll()->toArray()));
+class JsonHeaderMiddleware extends \Slim\Middleware
+{
+	public function call(){
+		$app = $this->app;
+
+		$this->next->call();
+
+		$app->response->headers['Content-Type'] = 'application/json';
+	}
+}
+
+$app->add(new \JsonHeaderMiddleware());
+
+$app->error(function (\Exception $e) use ($app) {
+	$json = [
+		'type' => get_class($e),
+		'code' => $e->getCode(),
+		'message'=>$e->getMessage(),
+	];
+	
+	//$json['file'] = $e->getFile();
+	//$json['line'] = $e->getLine();
+	//$json['trace'] = $e->getTrace();
+	
+	$app->response->write(json_encode($json));
 });
 
+$app->get('/:keyspace/query', function ($keyspace) use ($app, $db){
+	$db->setKeyspace($keyspace);
+	$cql = $app->request->get('cql');
+	$rows = $db->querySync($cql)->fetchAll();
+	
+	$app->response->write(json_encode($rows->toArray()));
+});
 
 $app->get('/:keyspace/:table', function ($keyspace, $table) use ($app, $db) {
-    $db->setKeyspace($keyspace);
-    $params = $app->request->get();
-    $select = FluentCQL\Query::select('*')->from($table);
-    $conditions = 0;
-    foreach($params as $columnName => $value){
-        if ($conditions ++ === 0)
-            $select->where("\"$columnName\" = ?", $value);
-        else
-            $select->and("\"$columnName\" = ?", $value);
-    }
-    $select->limit(10);
-    $preparedData = $db->prepare($select->assemble());
-    $strictValues = Cassandra\Request\Request::strictTypeValues($select->getBind(), $preparedData['metadata']['columns']);
-    $result = $db->executeSync($preparedData['id'], $strictValues);
-    $app->response->write(json_encode($result->fetchAll()->toArray()));
+	$db->setKeyspace($keyspace);
+	$params = $app->request->get();
+	
+	$select = FluentCQL\Query::select('*')->from($table);
+	
+	if (!empty($params)){
+		$conditions = [];
+		foreach($params as $columnName => $value)
+			$conditions[] = $columnName . ' = ?';
+		
+		$select->where(implode(' AND ', $conditions));
+	}
+	
+	$preparedData = $db->prepare($select->assemble());
+	$bind = [];
+	$index = 0;
+	foreach($params as $value){
+		$bind[] = Cassandra\Type\Base::getTypeObject($preparedData['metadata']['columns'][$index]['type'], $value);
+		++$index;
+	}
+	
+	$rows = $db->executeSync($preparedData['id'], $bind)->fetchAll();
+	
+	$app->response->write(json_encode($rows->toArray()));
+});
+
+$app->patch('/:keyspace/:table', function ($keyspace, $table) use ($app, $db) {
+	$db->setKeyspace($keyspace);
+	$params = $app->request->get();
+	$data = $app->request->patch();
+	
+	$assignments = [];
+	foreach($data as $columnName => $value)
+		$assignments[] = $columnName . ' = ?';
+	
+	$conditions = [];
+	foreach($params as $columnName => $value)
+		$conditions[] = $columnName . ' = ?';
+	
+	$query = FluentCQL\Query::update($table)
+		->set(implode(', ', $assignments))
+		->where(implode(' AND ', $conditions));
+	
+	$preparedData = $db->prepare($query->assemble());
+	
+	$bind = [];
+	$index = 0;
+	foreach($data as $value){
+		$bind[] = Cassandra\Type\Base::getTypeObject($preparedData['metadata']['columns'][$index]['type'], $value);
+		++$index;
+	}
+	foreach($params as $value){
+		$bind[] = Cassandra\Type\Base::getTypeObject($preparedData['metadata']['columns'][$index]['type'], $value);
+		++$index;
+	}
+	
+	$result = $db->executeSync($preparedData['id'], $bind);
+	
+	$app->response->write(json_encode($result->getData()));
 });
 
 $app->post('/:keyspace/:table', function ($keyspace, $table) use ($app, $db) {
-    // TODO
+	$db->setKeyspace($keyspace);
+	$data = $app->request->post();
+	
+	$query = FluentCQL\Query::insertInto($table)
+		->clause('(' . \implode(', ', \array_keys($data)) . ')')
+		->values('(' . \implode(', ', \array_fill(0, count($data), '?')) . ')');
+	
+	$preparedData = $db->prepare($query->assemble());
+	
+	$bind = [];
+	$index = 0;
+	foreach($data as $value){
+		$bind[] = Cassandra\Type\Base::getTypeObject($preparedData['metadata']['columns'][$index]['type'], $value);
+		++$index;
+	}
+	
+	$result = $db->executeSync($preparedData['id'], $bind);
+	
+	$app->response->write(json_encode($result->getData()));
 });
 
 $app->delete('/:keyspace/:table', function ($keyspace, $table) use ($app, $db) {
-    // TODO
+	$db->setKeyspace($keyspace);
+	$params = $app->request->get();
+	
+	$conditions = [];
+	foreach($params as $columnName => $value)
+		$conditions[] = $columnName . ' = ?';
+	
+	$select = FluentCQL\Query::delete()->from($table);
+	
+	$preparedData = $db->prepare($select->assemble());
+	$bind = [];
+	$index = 0;
+	foreach($params as $value){
+		$bind[] = Cassandra\Type\Base::getTypeObject($preparedData['metadata']['columns'][$index]['type'], $value);
+		++$index;
+	}
+	
+	$result = $db->executeSync($preparedData['id'], $bind);
+	
+	$app->response->write(json_encode($result->getData()));
 });
 
 $app->run();
